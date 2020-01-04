b4cae32d4d2722530d7a8485ca2381fc9ef744c0
Martin Krošlák <kroslakma@gmail.com>
2020-01-04 06:44:35 +0100

    Skip default values when serializing

--- src/types.rs
+++ src/types.rs
@@ -2,7 +2,7 @@
 ///
 /// The registry contains all the information contained in a certain version
 /// of the Vulkan specification, stored within a programmer-accessible format.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Registry(pub Vec<RegistryChild>);
 
 /// An element of the Vulkan registry.
@@ -10,22 +10,30 @@ pub struct Registry(pub Vec<RegistryChild>);
 pub enum RegistryChild {
     /// Comments are human-readable strings which contain registry meta-data.
     Comment(String),
+
     /// IDs of all known Vulkan vendors.
     VendorIds(VendorIds),
+
     /// List of supported Vulkan platforms.
     Platforms(Platforms),
+
     /// Known extension tags.
     Tags(Tags),
+
     /// Type definitions.
     ///
     /// Unlike OpenGL, Vulkan is a strongly-typed API.
     Types(Types),
+
     /// Enum definitions.
     Enums(Enums),
+
     /// Commands are the Vulkan API's name for functions.
     Commands(Commands),
+
     /// Feature level of the API, such as Vulkan 1.0 or 1.1
     Feature(Feature),
+
     /// Container for all published Vulkan specification extensions.
     Extensions(Extensions),
 }
@@ -34,16 +42,19 @@ pub type VendorIds = CommentedChildren<VendorId>;
 
 /// Unique identifier for a Vulkan vendor.
 ///
-/// Note: in newer versions of the Vulkan spec, this tag is not used,
-/// instead it has been replaced by the `VKVendorId` enum.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+/// Note: in newer versions of the Vulkan spec (1.1.79 and later), this tag is
+/// not used, instead it has been replaced by the `VKVendorId` enum.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct VendorId {
     /// Name of the vendor.
     pub name: String,
-    /// Human-readable description.
-    pub comment: Option<String>,
+
     /// The unique ID.
     pub id: u32,
+
+    /// Human-readable description.
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub comment: Option<String>,
 }
 
 pub type Platforms = CommentedChildren<Platform>;
@@ -52,14 +63,19 @@ pub type Platforms = CommentedChildren<Platform>;
 ///
 /// Most operating systems will have only one corresponding platform,
 /// but Linux has multiple (XCB, Wayland, etc.)
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+///
+/// Used in versions 1.1.70 and later.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Platform {
     /// Short identifier.
     pub name: String,
-    /// Human readable description of the platform.
-    pub comment: Option<String>,
+
     /// C macro name which is used to guard platform-specific definitions.
     pub protect: String,
+
+    /// Human readable description of the platform.
+    #[serde(default, skip_serializing_if = "is_default")]
+    pub comment: Option<String>,
 }
 
 pub type Tags = CommentedChildren<Tag>;
@@ -69,7 +85,7 @@ pub type Tags = CommentedChildren<Tag>;
 /// Some examples:
 /// - KHR for Khronos extensions
 /// - EXT for multi-vendor extensions
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Tag {
     /// The name of the tag, e.g. "KHR".
     pub name: String,
@@ -88,31 +104,58 @@ pub enum TypesChild {
     Comment(String),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Type {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub name: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub alias: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub api: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub requires: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub category: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub parent: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub returnedonly: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub structextends: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub spec: TypeSpec,
 }
 
 /// The contents of a type definition.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum TypeSpec {
+    None,
     Code(TypeCode),
     Members(Vec<TypeMember>),
-    None,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+impl Default for TypeSpec {
+    fn default() -> Self {
+        TypeSpec::None
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct TypeCode {
     pub code: String,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub markup: Vec<TypeCodeMarkup>,
 }
 
@@ -128,20 +171,38 @@ pub enum TypeCodeMarkup {
 pub enum TypeMember {
     /// Human-readable comment.
     Comment(String),
+
     /// A structure field definition.
     Definition(TypeMemberDefinition),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct TypeMemberDefinition {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub len: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub altlen: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub externsync: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub optional: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub noautovalidity: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub validextensionstructs: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub values: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub code: String,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub markup: Vec<TypeMemberMarkup>,
 }
 
@@ -153,14 +214,27 @@ pub enum TypeMemberMarkup {
     Comment(String),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Enums {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub name: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub kind: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub start: Option<i64>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub end: Option<i64>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub vendor: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub children: Vec<EnumsChild>,
 }
 
@@ -169,65 +243,103 @@ pub struct Enums {
 pub enum EnumsChild {
     /// Actual named enum.
     Enum(Enum),
+
     /// An unused range of enum values.
     Unused(Unused),
+
     /// Human-readable comment.
     Comment(String),
 }
 
 /// An unused range of enum values.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Unused {
     /// Beginning of the range.
     pub start: i64,
+
     /// Ending value of the range, if any.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub end: Option<i64>,
+
     /// Vendor who reserved this range.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub vendor: Option<String>,
+
     /// Human-readable description.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
 }
 
 /// An item of an enumeration type.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Enum {
     /// Name of this enum.
     pub name: String,
+
     /// Human-readable description.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub type_suffix: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub api: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub spec: EnumSpec,
 }
 
 /// An enum specifier, which assigns a value to the enum.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum EnumSpec {
+    None,
+
     Alias {
         alias: String,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         extends: Option<String>,
     },
+
     Offset {
         offset: i64,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         extends: String,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         extnumber: Option<i64>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         dir: bool,
     },
+
     /// Indicates an enum which is a bit flag.
     Bitpos {
         /// The bit to be set.
         bitpos: i64,
+
         /// Which structure this enum extends.
+        #[serde(default, skip_serializing_if = "is_default")]
         extends: Option<String>,
     },
+
     /// An enum value.
     Value {
         /// Hard coded value for an enum.
         value: String, // rnc says this is an Integer, but validates it as text, and that's what it sometimes really is.
+
         /// Which structure this enum extends.
+        #[serde(default, skip_serializing_if = "is_default")]
         extends: Option<String>,
     },
-    None,
+}
+
+impl Default for EnumSpec {
+    fn default() -> Self {
+        EnumSpec::None
+    }
 }
 
 pub type Commands = CommentedChildren<Command>;
@@ -237,53 +349,99 @@ pub type Commands = CommentedChildren<Command>;
 pub enum Command {
     /// Indicates this function is an alias for another one.
     Alias { name: String, alias: String },
+
     /// Defines a new Vulkan function.
     Definition(CommandDefinition),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct CommandDefinition {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub queues: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub successcodes: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub errorcodes: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub renderpass: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub cmdbufferlevel: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub pipeline: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub proto: NameWithType,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub params: Vec<CommandParam>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub alias: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub description: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub implicitexternsyncparams: Vec<String>,
 
+    #[serde(default, skip_serializing_if = "is_default")]
     pub code: String,
 }
 
 /// Parameter for this Vulkan function.
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct CommandParam {
     /// The expression which indicates the length of this array.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub len: Option<String>,
+
     /// Alternate description of the length of this parameter.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub altlen: Option<String>,
+
     /// Whether this parameter must be externally synchronised by the app.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub externsync: Option<String>,
+
     /// Whether this parameter must have a non-null value.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub optional: Option<String>,
+
     /// Disables automatic validity language being generated for this item.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub noautovalidity: Option<String>,
 
     /// The definition of this parameter.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub definition: NameWithType,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Feature {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub api: String,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub name: String,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub number: String,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub protect: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub children: Vec<FeatureChild>,
 }
 
@@ -291,35 +449,63 @@ pub type FeatureChild = ExtensionChild;
 
 pub type Extensions = CommentedChildren<Extension>;
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct Extension {
     /// Name of the extension.
     pub name: String,
+
     /// Human-readable description.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
     /// The unique index of this extension.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub number: Option<i64>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub protect: Option<String>,
+
     /// Which platform it works with, if any.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub platform: Option<String>,
+
     /// Tag name of the author.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub author: Option<String>,
+
     /// Contact information for extension author(s).
+    #[serde(default, skip_serializing_if = "is_default")]
     pub contact: Option<String>,
+
     /// The level at which the extension applies (instance / device).
+    #[serde(default, skip_serializing_if = "is_default")]
     pub ext_type: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub requires: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub requires_core: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub supported: Option<String>, // mk:TODO StringGroup?
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub deprecatedby: Option<String>,
+
     /// Whether this extension was promoted to core, and in which version.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub promotedto: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub obsoletedby: Option<String>,
 
     /// 'true' if this extension is released provisionally
     #[serde(default, skip_serializing_if = "is_default")]
     pub provisional: bool,
+
     /// The items which make up this extension.
+    #[serde(default, skip_serializing_if = "is_default")]
     pub children: Vec<ExtensionChild>,
 }
 
@@ -330,20 +516,38 @@ pub struct Extension {
 pub enum ExtensionChild {
     /// Indicates the items which this extension requires to work.
     Require {
+        #[serde(default, skip_serializing_if = "is_default")]
         api: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         profile: Option<String>,
+
         /// The extension which provides these required items, if any.
+        #[serde(default, skip_serializing_if = "is_default")]
         extension: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         feature: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         comment: Option<String>,
+
         /// The items which form this require block.
         items: Vec<InterfaceItem>,
     },
+
     /// Indicates the items this extension removes.
     Remove {
+        #[serde(default, skip_serializing_if = "is_default")]
         api: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         profile: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         comment: Option<String>,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         items: Vec<InterfaceItem>,
     },
 }
@@ -354,26 +558,38 @@ pub enum ExtensionChild {
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum InterfaceItem {
     Comment(String),
+
     Type {
         name: String,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         comment: Option<String>,
     },
+
     Enum(Enum),
+
     Command {
         name: String,
+
+        #[serde(default, skip_serializing_if = "is_default")]
         comment: Option<String>,
     },
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct NameWithType {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub type_name: Option<String>,
+
+    #[serde(default, skip_serializing_if = "is_default")]
     pub name: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct CommentedChildren<T> {
+    #[serde(default, skip_serializing_if = "is_default")]
     pub comment: Option<String>,
+
     pub children: Vec<T>,
 }
 
