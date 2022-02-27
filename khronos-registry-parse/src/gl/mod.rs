95791f677518744db759d869e17ec3bcdacf0d99
Michael Pollind <mpollind@gmail.com>
2022-02-27 15:51:56 -0800

    chore: fix import

--- khronos-registry-parse/src/gl/mod.rs
+++ khronos-registry-parse/src/gl/mod.rs
@@ -1,5 +1,5 @@
 
 mod parse;
 mod types;
-pub use parse::parse_file;
-pub use parse::parse_stream;
+pub use gl::parse::parse_file;
+pub use gl::parse::parse_stream;
--- khronos-registry-parse/src/vk/mod.rs
+++ khronos-registry-parse/src/vk/mod.rs
@@ -6,8 +6,9 @@ mod types;
 #[cfg(feature = "vkxml-convert")]
 extern crate vkxml;
 #[cfg(feature = "vkxml-convert")]
-pub use convert::parse_file_as_vkxml;
+pub use vk::convert::parse_file_as_vkxml;
 #[cfg(feature = "vkxml-convert")]
-pub use convert::parse_stream_as_vkxml;
-pub use parse::parse_file;
-pub use parse::parse_stream;
+pub use vk::convert::parse_stream_as_vkxml;
+
+pub use vk::parse::parse_file;
+pub use vk::parse::parse_stream;
