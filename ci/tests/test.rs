48b9b3586146ee449d0c016592e2ed6229a7acba
Martin Krošlák <kroslakma@gmail.com>
2026-03-07 00:38:27 +0100

    Add tests for new versions

--- ci/tests/test.rs
+++ ci/tests/test.rs
@@ -497,3 +497,7 @@ test_version! {test_v1_4_338, 1, 4, 338, "/xml"}
 test_version! {test_v1_4_339, 1, 4, 339, "/xml"}
 test_version! {test_v1_4_340, 1, 4, 340, "/xml"}
 test_version! {test_v1_4_341, 1, 4, 341, "/xml"}
+test_version! {test_v1_4_342, 1, 4, 342, "/xml"}
+test_version! {test_v1_4_343, 1, 4, 343, "/xml"}
+test_version! {test_v1_4_344, 1, 4, 344, "/xml"}
+test_version! {test_v1_4_345, 1, 4, 345, "/xml"}
